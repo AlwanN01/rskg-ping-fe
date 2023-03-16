5e2e8747733038a46d5c2369e50b6e43da0662c4
AlwanN01 <alwannabawi112233@gmail.com>
2023-03-16 21:02:15 +0700

    add paginations

--- src/app/ping/Ping.tsx
+++ src/app/ping/Ping.tsx
@@ -2,36 +2,43 @@
 import { useRouter } from 'next/navigation'
 import { usePing } from './hooks/usePing'
 import { type DataHost } from './page'
+import { Container, Pagination, Table, NumberInput, Group } from '@mantine/core'
 
 type IPing = { data: DataHost[]; children?: React.ReactNode }
 export default function Ping({ data }: IPing) {
-  const { hosts } = usePing(data)
+  const { hosts, limit, setLimit, activePage, setActivePage, totalPage } = usePing(data)
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>HostName</th>
-          <th>User</th>
-          <th>Divisi</th>
-          <th>Status</th>
-          <th>Updated At</th>
-        </tr>
-      </thead>
-      <tbody>
-        {hosts.map(host => {
-          const updateAt = host.updatedAt && new Date(host.updatedAt).toLocaleString('id-ID', { dateStyle: 'full', timeStyle: 'long' })
-          return (
-            <tr key={host.id}>
-              <td>{host.hostName}</td>
-              <td>{host.user}</td>
-              <td>{host.divisi}</td>
-              <td>{host.status || 'unknown status'}</td>
-              <td>{updateAt || 'unknown date'}</td>
-            </tr>
-          )
-        })}
-      </tbody>
-    </table>
+    <Container size={'xl'}>
+      <Table>
+        <thead>
+          <tr>
+            <th>HostName</th>
+            <th>User</th>
+            <th>Divisi</th>
+            <th>Status</th>
+            <th>Updated At</th>
+          </tr>
+        </thead>
+        <tbody>
+          {hosts.map(host => {
+            const updateAt = host.updatedAt && new Date(host.updatedAt).toLocaleString('id-ID', { dateStyle: 'full', timeStyle: 'long' })
+            return (
+              <tr key={host.id}>
+                <td>{host.hostName}</td>
+                <td>{host.user}</td>
+                <td>{host.divisi}</td>
+                <td>{host.status || 'unknown status'}</td>
+                <td>{updateAt || 'unknown date'}</td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </Table>
+      <Group>
+        <NumberInput type='number' radius={'sm'} w={60} min={1} max={Math.ceil(data.length / limit)} value={10} onChange={setLimit} />
+        <Pagination page={activePage} onChange={setActivePage} total={totalPage} />
+      </Group>
+    </Container>
   )
 }
 
--- src/app/ping/hooks/usePing.tsx
+++ src/app/ping/hooks/usePing.tsx
@@ -6,13 +6,22 @@ import type { DataHost } from '../page'
 type Status = { status: string; updatedAt: Date }
 export interface Host extends DataHost, Partial<Status> {}
 
-const initState = { hosts: [] as Host[] }
+const initState = { hosts: [] as Host[], activePage: 1, limit: 10 }
 const _usePing = createStore(initState, null, { nameStore: 'PING STORE' })
 const { setState, getState } = _usePing
 
 export const usePing = (_hosts: Host[]) => {
   useMemo(() => void setState(state => void (state.hosts.length == 0 && (state.hosts = _hosts)), false, { type: 'Init Host Data' }), [_hosts])
   const { hosts } = _usePing()
+  const { activePage, limit } = getState()
+  const getPageData = () => {
+    const start = (activePage - 1) * limit
+    const end = start + limit
+    return hosts.slice(start, end)
+  }
+  const totalPage = useMemo(() => Math.ceil(hosts.length / limit), [hosts.length, limit])
+  const setActivePage = (page: number) => setState({ activePage: page })
+  const setLimit = (val: number) => setState({ limit: val > 0 ? val : 1, activePage: 1 })
   useEffect(() => {
     socket.connect()
     for (const { hostName } of _hosts) {
@@ -35,5 +44,5 @@ export const usePing = (_hosts: Host[]) => {
     }
     return () => void socket.disconnect()
   }, [_hosts])
-  return { hosts }
+  return { hosts: getPageData(), activePage, setActivePage, limit, setLimit, totalPage }
 }
